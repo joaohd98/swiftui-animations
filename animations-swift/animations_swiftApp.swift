b84d7249edb44ca9267abf428614451699ae77e7
João Damazio <joao.damazio@fiap.com.br>
2024-06-15 00:09:38 -0300

    Initial Commit

---
+++ animations-swift/animations_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  animations_swiftApp.swift
+//  animations-swift
+//
+//  Created by João Damazio on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct animations_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
